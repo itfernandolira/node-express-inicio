a9009b1fa978e50780e20b46fbb14ea896bafa40
Fernando Lira <it.fernandolira@gmail.com>
2021-10-05 10:20:15 +0100

    Vários parametros

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ app.get("/blog/:artigo?",function(req, res){
     }
 })
 
+app.get("/ola/:nome/:empresa", function(req, res){
+    // REQ => DADOS ENVIADOS PELO USUÁRIO
+    // RES => RESPOSTA QUE VAI SER ENVIADA PARA O USUÁRIO
+    var nome = req.params.nome;
+    var empresa = req.params.empresa;
+    res.send("<h1>Oi " + nome +  " do " + empresa + " </h1>");
+});
+
 app.get("/canal/youtube",function(req,res){
     res.send("<h1>Bem vindo ao CANAL de YOUTUBE</h1>");
 });
