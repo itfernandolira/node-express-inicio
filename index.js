a4b5c98ebcc352d6ac53afcd5c71140c5faab44f
Fernando Lira <it.fernandolira@gmail.com>
2021-10-05 10:15:00 +0100

    Parametros

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ app.get("/",function(req,res){
     res.send("<h1>Bem vindo ao NODEjs</h1>");
 });
 
-app.get("/blog",function(req,res){
-    res.send("<h1>Bem vindo ao BLOG</h1>");
-});
+app.get("/blog/:artigo?",function(req, res){
+
+    var artigo = req.params.artigo;
+
+    if(artigo){
+        res.send("<h2>Artigo: " + artigo + " </h2>");
+    }else{
+        res.send("<h3>Bem vindo ao meu blog!: www.flag.pt</h3>");
+    }
+})
 
 app.get("/canal/youtube",function(req,res){
     res.send("<h1>Bem vindo ao CANAL de YOUTUBE</h1>");
