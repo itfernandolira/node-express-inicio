e97d31d6d1b63a5ce3a36e3e0a9eeba772b5e3aa
Fernando Lira <it.fernandolira@gmail.com>
2021-10-05 09:51:21 +0100

    Rotas

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require("express"); // Importar o express
 const app = express(); // Iniciar o express
 
+app.get("/",function(req,res){
+    res.send("<h1>Bem vindo ao NODEjs</h1>");
+});
+
+app.get("/blog",function(req,res){
+    res.send("<h1>Bem vindo ao BLOG</h1>");
+});
+
+app.get("/canal/youtube",function(req,res){
+    res.send("<h1>Bem vindo ao CANAL de YOUTUBE</h1>");
+});
+
 app.listen(4000,function(erro){
     if(erro){
         console.log("Ocorreu um erro!");
